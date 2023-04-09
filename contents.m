25dcaaaa4d045feabf70cdc004d5320f441a77f1
Giampiero Campa <122689018+giampy1969@users.noreply.github.com>
2023-04-09 16:49:25 -0400

    Add files via upload

---
+++ contents.m
@@ -0,0 +1,16 @@
+% Digital Equivalents Library
+% Version 1.3 (R11.1,R12.1)  1-Apr-2002
+% 
+% Giampiero Campa
+% 
+%
+% LIBRARY:
+%
+% dequiv       : open the dequiv simulink library.
+% 
+%
+% EXAMPLES :
+%
+% digfilt     : main example.
+% multifil    : multichannel filters example. 
+%
